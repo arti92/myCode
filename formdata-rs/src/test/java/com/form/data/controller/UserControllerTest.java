77d339a5d496d29044fddf0239579e58a0e6ec49
arti.jadhav <arti.jadhav@ril.com>
2022-12-01 19:36:09 +0530

    apk changes for local

--- formdata-rs/src/test/java/com/form/data/controller/UserControllerTest.java
+++ formdata-rs/src/test/java/com/form/data/controller/UserControllerTest.java
@@ -3,7 +3,11 @@ package com.form.data.controller;
 import com.form.data.model.User;
 import com.form.data.repository.UsersRepository;
 import com.form.data.service.UserService;
-import mockit.*;
+
+import mockit.Expectations;
+import mockit.Injectable;
+import mockit.Mocked;
+import mockit.Tested;
 import mockit.integration.junit4.JMockit;
 import org.junit.Assert;
 import org.junit.Test;
@@ -24,11 +28,14 @@ public class UserControllerTest {
     @Tested
     private UserController userController;
 
-   /* @Test
-    public void isUserUpdate(@Mocked Long id, @Mocked User userDetails) {
+  /* @Test
+    public void isUserUpdate() {
 
+       User userDetails = new User();
+       userDetails.setFirstName("a");
+       userDetails.setLastName("last");
 
-        Long finalId = id;
+        Long finalId = new Long(1234);
         finalId = new Long(123);
         Long finalId1 = finalId;
         new Expectations() {{
@@ -45,9 +52,6 @@ public class UserControllerTest {
         }};
         Boolean isUpdate = userController.isUserUpdate(finalId, userDetails);
         Assert.assertEquals("OK", isUpdate);
-        new FullVerifications(userService) {
-        };
-        new FullVerifications(userDetails) {
-        };
+
     }*/
 }
--- formdata-rs/src/test/java/com/form/data/service/UserServiceTest2.java
+++ formdata-rs/src/test/java/com/form/data/service/UserServiceTest2.java
@@ -1,15 +1,10 @@
 package com.form.data.service;
 
-import com.form.data.controller.UserController;
 import com.form.data.model.User;
 import com.form.data.repository.UsersRepository;
-import mockit.Expectations;
-import mockit.FullVerifications;
-import mockit.Injectable;
-import mockit.Tested;
+import mockit.*;
 import mockit.integration.junit4.JMockit;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,28 +12,58 @@ import org.junit.runner.RunWith;
  * @author Arti.Jadhav
  */
 @RunWith(JMockit.class)
-public class UserServiceTest {
-
+public class UserServiceTest2 {
 
     @Injectable
     private UsersRepository userDao;
 
     @Tested
     private UserService userService;
+/*
+    private Mockery mockContext;
+
+
+    @Before
+    public void setup() {
+        mockContext = new Mockery() {{
+            setImposteriser(ClassImposteriser.INSTANCE);
+        }};
+    }
+    @After
+    public void tearDown() {
+        mockContext.assertIsSatisfied();
+        mockContext = null;
+    }*/
 
     @Test
-    public void isUser(){
+    public void isUser(@Mocked User user1) {
         User user = new User();
         user.setFirstName("a");
-        user.setLastName("last");
+        // user.setLastName("last");
+
         new Expectations() {{
-            userDao.save(withAny(user)); result = user;
+            userDao.save(withAny(user));
+            result = user;
         }};
+      /*  new Verifications(){{
+            userDao.save(with(user));times =1;
+
+        }};*/
+        /*
 
         Boolean isUser = userService.isUpdate(user);
         System.out.println("isUser:: "+isUser);
         Assert.assertTrue("User id not present ",isUser);
-        new FullVerifications(user) {};
+        new FullVerifications(user) {};*/
+     /*   mockContext.checking(new Expectations() {{
+            allowing(userDao).save(user);
+            will(returnValue(user));
+        }});*/
+        Boolean isUser = userService.isUpdate(user);
+        System.out.println("isUser:: " + isUser);
+        Assert.assertTrue("User id not present ", isUser);
+        new FullVerifications(user) {
+        };
     }
 
 
